30a021ec1c5d0832264854579b51545f83b71e94
Marco Bogner <marcobogner@gmail.com>
2021-09-29 18:46:25 +0200

    Add global styles

--- pages/_app.js
+++ pages/_app.js
@@ -10,6 +10,21 @@ function MyApp({ Component, pageProps }) {
         <link rel="apple-touch-icon" href="/icon-apple-touch.png" />
         <link rel="manifest" href="/manifest.json" />
       </Head>
+      <Global
+        styles={css`
+          body {
+            *,
+            *::before,
+            *::after {
+              box-sizing: border-box;
+            }
+            font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+              Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue',
+              sans-serif;
+            margin: 0;
+          }
+        `}
+      />
       <Component {...pageProps} />
     </>
   );
