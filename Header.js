84f7472c18d5810942c8ca2fd16b4a141bc192fe
Marco Bogner <marcobogner@gmail.com>
2021-09-29 17:31:17 +0200

    Add babel and emotion config

--- Header.js
+++
@@ -1,9 +0,0 @@
-import { css } from '@emotion/react';
-
-const headerStyles = css`
-  width: 100vw;
-`;
-
-export default function Header() {
-  return <div css={headerStyles}>This is the header.</div>;
-}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import { css } from '@emotion/react';
 import Head from 'next/head';
 import Image from 'next/image';
-import Header from '../Header';
+import Link from 'next/link';
+import Header from '../public/Header';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
