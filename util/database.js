b655caa6997e9500c803b955ec82dacc23aae1ed
Marco Bogner <marcobogner@gmail.com>
2021-09-29 15:31:14 +0200

    Add js database with burritos

---
+++ util/database.js
@@ -0,0 +1,19 @@
+const burritos = [
+  {
+    name: 'Beef Burrito',
+    desc: 'This is an amazing beef burrito.',
+    price: 23,
+    img: 'placeholder',
+    ingredients: {
+      meat: true,
+      cheese: true,
+      beans: true,
+      lettuce: false,
+      guacamole: false,
+      salsa: true,
+      sourcream: true,
+      tomato: false,
+      coriander: false,
+    },
+  },
+];
