abeacd00dce14129d262d05831c6f1855a62b293
Marco Bogner <marcobogner@gmail.com>
2021-09-29 18:14:10 +0200

    Add layout component

---
+++ Components/Footer.js
@@ -0,0 +1,66 @@
+import { css } from '@emotion/react';
+import Link from 'next/link';
+
+const header = css`
+  background-color: #fef8f0;
+  height: 88px;
+  width: 100vw;
+  font-family: 'Inter' sans-seri;
+  align-items: center;
+`;
+
+const headerContent = css`
+  margin: auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  max-width: 1280px;
+  padding: 24px 32px;
+  font-size: 1rem;
+  line-height: 1.5rem;
+  font-weight: 500;
+  color: rgb(55, 65, 81);
+`;
+
+const button = css`
+  display: flex;
+  justify-content: center;
+  color: rgb(184, 98, 0);
+  background-color: rgb(255, 226, 194);
+  text-decoration: none;
+  font-size: 0.875rem;
+  font-weight: bold;
+  line-height: 1.25rem;
+  font-family: 'Inter' sans-serif;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 6px;
+  cursor: pointer;
+
+  :hover {
+    transform: scale(1.05) perspective(1px);
+    transition-duration: 100ms;
+    background-color: rgb(255, 207, 153);
+  }
+`;
+
+export default function Footer() {
+  return (
+    <div css={header}>
+      <div css={headerContent}>
+        <nav>
+          <ul>
+            <li>
+              <Link href="/">
+                <a>Home</a>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <a href="https://www.google.at" class="buttonLink">
+          <button css={button}>Shopping cart</button>
+        </a>
+      </div>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import { css } from '@emotion/react';
 import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
-import Header from '../public/Header';
+import Layout from '../Components/Layout';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
@@ -18,42 +18,43 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <Header />
-        <h1 className={styles.title}>Welcome to Burrito.me</h1>
+        <Layout>
+          <h1 className={styles.title}>Welcome to Burrito.me</h1>
 
-        <p className={styles.description}>
-          Burritos are what happens when your food hugs itself.
-        </p>
+          <p className={styles.description}>
+            Burritos are what happens when your food hugs itself.
+          </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          <div className={styles.grid}>
+            <a href="https://nextjs.org/docs" className={styles.card}>
+              <h2>Documentation &rarr;</h2>
+              <p>Find in-depth information about Next.js features and API.</p>
+            </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+            <a href="https://nextjs.org/learn" className={styles.card}>
+              <h2>Learn &rarr;</h2>
+              <p>Learn about Next.js in an interactive course with quizzes!</p>
+            </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+            <a
+              href="https://github.com/vercel/next.js/tree/master/examples"
+              className={styles.card}
+            >
+              <h2>Examples &rarr;</h2>
+              <p>Discover and deploy boilerplate example Next.js projects.</p>
+            </a>
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+            <a
+              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+              className={styles.card}
+            >
+              <h2>Deploy &rarr;</h2>
+              <p>
+                Instantly deploy your Next.js site to a public URL with Vercel.
+              </p>
+            </a>
+          </div>
+        </Layout>
       </main>
 
       <footer className={styles.footer}>
