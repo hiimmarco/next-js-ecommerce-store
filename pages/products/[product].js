4d084005cabdcae6d0de5dcb21ebbf7a08e4bbea
Marco Bogner <marcobogner@gmail.com>
2021-10-05 09:25:32 +0200

    Add counter

--- pages/products/[product].js
+++ pages/products/[product].js
@@ -1,6 +1,7 @@
 import { css } from '@emotion/react';
 import Head from 'next/head';
 import Image from 'next/image';
+import { useState } from 'react';
 import Layout from '../../Components/Layout';
 
 const wrapper = css`
@@ -20,8 +21,12 @@ const buttons = css`
 `;
 
 export default function Productdetail(props) {
+  // This is for getting the query from the URL in the frontnend
   /*   const router = useRouter();
   const { product } = router.query; */
+
+  const [count, setCount] = useState(0);
+
   return (
     <div>
       <Layout>
@@ -37,12 +42,25 @@ export default function Productdetail(props) {
             height={600}
           />
 
-          <h1>Product Detail Page for {props.singleBurrito.name} </h1>
+          <h1>{props.singleBurrito.name} </h1>
           <p>{props.singleBurrito.desc}</p>
           <div css={buttons}>
-            <button>-</button>
-            <p>1</p>
-            <button>+</button>
+            <button
+              onClick={() => {
+                setCount(count - 1);
+              }}
+            >
+              -
+            </button>
+            <p>{count}</p>
+            <button
+              onClick={() => {
+                setCount(count + 1);
+              }}
+            >
+              +
+            </button>
+            <button>Add to cart</button>
           </div>
         </div>
       </Layout>
@@ -50,6 +68,7 @@ export default function Productdetail(props) {
   );
 }
 
+// Get data from database and reduce to one object (by finding the unique ID of the object via the URL)
 export async function getServerSideProps(context) {
   const { burritos } = await import('../../util/database');
 
